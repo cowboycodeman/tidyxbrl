0e7442233f51605daa73d77cbc16ef07c6cd8e35
cowboycodeman <ryancmcdonald@gmail.com>
2021-11-15 21:56:30 -0700

    removd broken test

--- tests/test_edgar_frames.py
+++ tests/test_edgar_frames.py
@@ -1,5 +1,3 @@
 import tidyxbrl
 
 tidyxbrl.edgar_frames(urldescriptor = 'us-gaap/NonoperatingIncomeExpense/USD/CY2019Q1I')
-
-tidyxbrl.edgar_frames(urldescriptor = 'us-gaap/OtherComprehensiveIncome/USD/CY2019Q1I')
