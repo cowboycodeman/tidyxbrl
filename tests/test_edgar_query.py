8f7ac65ac930017e10ba3c5994bc57aab5634801
cowboycodeman <85976620+cowboycodeman@users.noreply.github.com>
2024-08-06 11:35:33 -0600

    Update test_edgar_query.py

--- tests/test_edgar_query.py
+++ tests/test_edgar_query.py
@@ -1,12 +1,17 @@
-import tidyxbrl
+import sys
+import os
+import pytest
+from tidyxbrl import edgar_cik, edgar_query
 
 def test_edgar_query():
-    ms_sub = tidyxbrl.edgar_query('0000789019', query_type='submissions')
-    ms_concept = tidyxbrl.edgar_query('0000789019', query_type='companyconcept', queryextension='/us-gaap/AccountsPayableCurrent.json')
-    ms_facts = tidyxbrl.edgar_query('0000789019', query_type='companyfacts')
+    ms_sub = edgar_query('0000789019', query_type='submissions')
+    ms_concept = edgar_query('0000789019', query_type='companyconcept', queryextension='/us-gaap/AccountsPayableCurrent.json')
+    ms_facts = edgar_query('0000789019', query_type='companyfacts')
 
-    companycik = tidyxbrl.edgar_cik("ZILLOW GROUP, INC.")
+    companycik = edgar_cik("ZILLOW GROUP, INC.")
     desiredcorp = str(companycik[companycik.company.str.contains("ZILLOW GROUP, INC.")]['cik_str'].unique()[0])
-    zillow_sub = tidyxbrl.edgar_query(desiredcorp, query_type='submissions')
-    zillow_concept = tidyxbrl.edgar_query(desiredcorp, query_type='companyconcept', queryextension='/us-gaap/AccountsPayableCurrent.json')
-    zillow_facts = tidyxbrl.edgar_query(desiredcorp, query_type='companyfacts')
+    zillow_sub = edgar_query(desiredcorp, query_type='submissions')
+    zillow_concept = edgar_query(desiredcorp, query_type='companyconcept', queryextension='/us-gaap/AccountsPayableCurrent.json')
+    zillow_facts = edgar_query(desiredcorp, query_type='companyfacts')
+
+# %%
