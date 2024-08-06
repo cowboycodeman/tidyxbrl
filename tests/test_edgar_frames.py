ae2ae098b6ba7c17ab1528c08a9fa1ac5bc37c96
cowboycodeman <85976620+cowboycodeman@users.noreply.github.com>
2024-08-06 11:34:55 -0600

    Update test_edgar_frames.py

--- tests/test_edgar_frames.py
+++ tests/test_edgar_frames.py
@@ -1,10 +1,16 @@
-import tidyxbrl
+"""
+
+"""
+# %%
+# import tidyxbrl
+import sys
+import os
 import pytest
+from tidyxbrl import edgar_frames
 
-
-# %% 
+# %%
 def test_edgar_frames():
-    tidyxbrl.edgar_frames(urldescriptor='us-gaap/NonoperatingIncomeExpense/USD/CY2019Q1I')
+    edgar_frames(urldescriptor='us-gaap/NonoperatingIncomeExpense/USD/CY2019Q1I')
 
 
 # %%
