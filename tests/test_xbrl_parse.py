5b1e5fede74ed769144e22b50d0389cb25cf5eec
cowboycodeman <85976620+cowboycodeman@users.noreply.github.com>
2024-08-06 11:35:57 -0600

    Update test_xbrl_parse.py

--- tests/test_xbrl_parse.py
+++ tests/test_xbrl_parse.py
@@ -1,14 +1,16 @@
+import sys
+import os
 import pytest
-import tidyxbrl 
+from tidyxbrl import xbrl_parse
 
 def test_xbrl_parse():
     applesecweburl = "https://www.sec.gov/Archives/edgar/data/320193/000032019321000010/aapl-20201226_htm.xml"
     teslasecweburl = "https://www.sec.gov/Archives/edgar/data/1318605/000156459020047486/tsla-10q_20200930_htm.xml"
     ibmsecweburl = "https://www.sec.gov/Archives/edgar/data/51143/000155837020001334/ibm-20191231x10k2af531_htm.xml"
 
-    appledata = tidyxbrl.xbrl_parse(applesecweburl)
-    tesladata = tidyxbrl.xbrl_parse(teslasecweburl)
-    ibmdata = tidyxbrl.xbrl_parse(ibmsecweburl)
+    appledata = xbrl_parse(applesecweburl)
+    tesladata = xbrl_parse(teslasecweburl)
+    ibmdata = xbrl_parse(ibmsecweburl)
 
     # Add your assertions here
     assert appledata is not None
