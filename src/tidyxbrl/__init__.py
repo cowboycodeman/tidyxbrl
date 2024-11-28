cb995b19f5f0af51be44746002e0440b8cb6ca2b
Ryan McDonald <ryancmcdonald@gmail.com>
2024-11-28 14:38:27 -0700

    fixing import issue

--- src/tidyxbrl/__init__.py
+++ src/tidyxbrl/__init__.py
@@ -1,6 +1,10 @@
-from .xbrl_apikey import *
-from .xbrl_query import *
-from .xbrl_parse import *
-from .edgar_query import *
-from .edgar_cik import *
-from .edgar_frames import *
+# %%
+# __init__.py
+from tidyxbrl.xbrl_apikey import *
+from tidyxbrl.xbrl_query import *
+from tidyxbrl.xbrl_parse import *
+from tidyxbrl.edgar_query import *
+from tidyxbrl.edgar_cik import *
+from tidyxbrl.edgar_frames import *
+from src.config.default_headers import *
+# %%
